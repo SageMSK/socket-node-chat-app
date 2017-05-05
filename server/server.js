d60a00de4b869bd88c71b8491a24039aa25580cb
Min Soo Kim <minskim92@gmail.com>
2017-05-05 12:44:53 -0400

    Added message

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+io.on('connection', (socket) => {
+  console.log('New user connected.');
 
-app.listen(PORT, () => {
+  socket.on('createMessage', message => {
+    console.log('createEmail', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected.');
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`)
 });

c30df9fa24d287a936ee1b9a332904751f215c77
Min Soo Kim <minskim92@gmail.com>
2017-05-10 15:16:33 -0400

    added mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,22 +11,26 @@ $(document).ready(function () {
   
   socket.on('newMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = $('<li></li>');
-    li.text(`${message.from} (${formattedTime}): ${message.message}`);
+    let template = $('#message-template').html();
+    let html = Mustache.render(template, {
+      message: message.message,
+      from: message.from,
+      createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
   });
 
   socket.on('newLocationMessage', function (message) {
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = $('<li></li>');
-    let a = $('<a target="_blank">My current location</a>');
+    let locationMessageTemplate = $('#location-message-template').html();
+    let html = Mustache.render(locationMessageTemplate, {
+      from: message.from,
+      url: message.url,
+      createdAt: formattedTime
+    });
 
-    li.text(`${message.from} (${formattedTime}): `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    $('#messages').append(li);
+    $('#messages').append(html);
   });
 
 
